7a49c7d113394ea5a50af0d44d391b894180ff8b
Nate Bird <natebird@gmail.com>
2016-01-11 15:20:31 -0500

    Add section about classes to Object-Oriented Swift playground.

--- Object-Oriented Swift.playground/Contents.swift
+++ Object-Oriented Swift.playground/Contents.swift
@@ -45,17 +45,17 @@ let myBook = Book(title: "Animal Farm", author: "George Orwell", price: 6.00)
 
 
 // Code Challenge
-struct Person {
-  let firstName: String
-  let lastName: String
-  
-  func getFullName() -> String {
-    return "\(firstName) \(lastName)"
-  }
-}
-
-let nate = Person(firstName: "Nate", lastName: "Bird")
-let fullName = nate.getFullName()
+//struct Person {
+//  let firstName: String
+//  let lastName: String
+//  
+//  func getFullName() -> String {
+//    return "\(firstName) \(lastName)"
+//  }
+//}
+//
+//let nate = Person(firstName: "Nate", lastName: "Bird")
+//let fullName = nate.getFullName()
 
 
 // Instance Methods
@@ -82,6 +82,176 @@ struct RGBColor {
 }
 
 
+// Classes
+class Enemy {
+  var life: Int = 2
+  let position: Point
+  
+  init(x: Int, y: Int) {
+    self.position = Point(x: x, y: y)
+  }
+  
+  func decreaseHealth(factor: Int) {
+    life -= factor
+  }
+}
 
+// Code Challenge
+class Shape {
+  var numberOfSides: Int
+  
+  init(numberOfSides: Int) {
+    self.numberOfSides = numberOfSides
+  }
+}
+
+let someShape = Shape(numberOfSides: 5)
+
+
+class Tower {
+  let position: Point
+  var range: Int = 1
+  var strength: Int = 1
+  
+  init(x: Int, y: Int) {
+    self.position = Point(x: x, y: y)
+  }
+  
+  func fireAtEnemy(enemy: Enemy) {
+    if inRange(self.position, range: self.range, target: enemy.position) {
+      while enemy.life > 0 {
+        enemy.decreaseHealth(self.strength)
+        print("Enemy hit!")
+        
+        if enemy.life == 0 {
+          print("Enemy Vanguished")
+        }
+      }
+    } else {
+      print("The enemy is out of range!")
+    }
+  }
+  
+  func inRange(position: Point, range: Int, target: Point) -> Bool {
+    let availablePositions = position.surroundingPoints(withRange: range)
+    
+    for point in availablePositions {
+      if (point.x == target.x) && (point.y == target.y) {
+        // Immediately exits for loop
+        return true
+      }
+    }
+    return false
+  }
+}
+
+class SuperEnemy: Enemy {
+  let isSuper: Bool = true
+  
+  override init(x: Int, y: Int) {
+    super.init(x: x, y: y)
+    self.life = 10
+  }
+}
+
+class LaserTower: Tower {
+  
+  override init(x: Int, y: Int) {
+    super.init(x: x, y: y)
+    self.strength = 100
+    self.range = 100
+  }
+  
+  override func fireAtEnemy(enemy: Enemy) {
+    while enemy.life > 0 {
+      enemy.decreaseHealth(self.strength)
+      print("Enemy hit!")
+    }
+    print("Enemy Vanguished")
+  }
+}
+
+
+let tower = Tower(x: 0, y: 0)
+let enemy = Enemy(x: 1, y: 1)
+let superEnemy = SuperEnemy(x: 2, y: 2)
+superEnemy.life
+
+tower.fireAtEnemy(enemy)
+
+
+
+// Code Challenge
+struct Location {
+  let latitude: Double
+  let longitude: Double
+}
+
+class Business {
+  let name: String
+  let location: Location
+  
+  init(name: String, latitude: Double, longitude: Double) {
+    self.name = name
+    self.location = Location(latitude: latitude, longitude: longitude)
+  }
+}
+
+let someBusiness = Business(name: "Pringles", latitude: 12.3, longitude: 34.5)
+
+// Inheritance
+
+// Code Challenge
+
+class Vehicle {
+  var numberOfDoors: Int
+  var numberOfWheels: Int
+  
+  init(withDoors doors: Int, andWheels wheels: Int) {
+    self.numberOfDoors = doors
+    self.numberOfWheels = wheels
+  }
+}
+
+// Enter your code below
+
+class Car: Vehicle {
+  let numberOfSeats: Int = 4
+  
+  override init(withDoors doors: Int, andWheels wheels: Int) {
+    super.init(withDoors: doors, andWheels: wheels)
+  }
+}
+
+let someCar = Car(withDoors: 4, andWheels: 4)
+
+
+///////
+class Person {
+  let firstName: String
+  let lastName: String
+  
+  init(firstName: String, lastName: String) {
+    self.firstName = firstName
+    self.lastName = lastName
+  }
+  
+  func getFullName() -> String {
+    return "\(firstName) \(lastName)"
+  }
+}
+
+class Doctor: Person {
+  override init(firstName: String, lastName: String) {
+    super.init(firstName: firstName, lastName: lastName)
+  }
+  
+  override func getFullName() -> String {
+    return "Dr. \(lastName)"
+  }
+}
+
+let someDoctor = Doctor(firstName: "Kelly", lastName: "Slater")
+someDoctor.getFullName()
 
 
