309cfac5d60c5418b1201be86ba27dd229d6c09a
Nate Bird <natebird@gmail.com>
2016-01-22 14:03:37 -0500

    renamed to just enums

--- Swift Enumerations.playground/Contents.swift
+++ Swift Enumerations.playground/Contents.swift
@@ -1,4 +1,4 @@
-//: Enums and Optionals
+//: Enums
 
 let week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
@@ -124,10 +124,45 @@ enum MobilePhone {
 let iPhone = MobilePhone.iPhone("6S")
 
 
+import Foundation
+
+enum UIBarButtonStyle {
+  case Done
+  case Plain
+  case Bordered
+}
+
+class UIBarButtonItem {
+  
+  var title: String?
+  let style: UIBarButtonStyle
+  var target: AnyObject?
+  var action: Selector
+  
+  init(title: String?, style: UIBarButtonStyle, target: AnyObject?, action: Selector) {
+    self.title = title
+    self.style = style
+    self.target = target
+    self.action = action
+  }
+}
 
 
+enum Button {
+  case Done(String)
+  case Edit(String)
+  
+  func toUIBarButtonItem() -> UIBarButtonItem {
+    switch self {
+    case .Done("Done"): return UIBarButtonItem(title: "Done", style: UIBarButtonStyle.Done, target: nil, action: nil)
+    case .Edit("Edit"): return UIBarButtonItem(title: "Edit", style: UIBarButtonStyle.Plain, target: nil, action: nil)
+    default: return UIBarButtonItem(title: "Uknown", style: UIBarButtonStyle.Bordered, target: nil, action: nil)
+    }
+  }
+}
 
-
+let done = Button.Done("Done")
+let doneButton = done.toUIBarButtonItem()
 
 
 
